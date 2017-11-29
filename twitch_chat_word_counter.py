66aea80968f8877a5c60bd2d88799d12a0a7c417
sebastien tremblay <yanns.tremblay@gmail.com>
2017-11-28 17:31:26 -0800

    Updated docstrings

--- twitch_chat_word_counter.py
+++ twitch_chat_word_counter.py
@@ -13,6 +13,20 @@ class ChattyLog:
     def to_dataframe(self, raw_contents):
         """
         Read file buffer into pandas DataFrame
+
+        Parameters
+        ----------
+        raw_contents : list
+            Raw output of logfile file buffer. No processing has been done at
+            this point. Read in with utf-8 encoding to best deal with variety
+            of characters.
+
+        Returns
+        -------
+        df : pandas.DataFrame Parsed logfile with 3 column names: timestamp,
+            username, and content. NaN values are replaced with empty strings,
+            extraneous chat info is stripped as well (log open and close
+            lines, mod announcements and bans.)
         """
         s = pd.Series(raw_contents)
         s = s[s.str.contains('<')]
@@ -21,11 +35,32 @@ class ChattyLog:
         df['timestamp'] = pd.to_datetime(df['timestamp'])
         df['username'] = s.str.extract('<(.*?)>', expand=True)
         df['content'] = s.str.extract('> (.*?)$', expand=True)
-        df['content'] = df['content'].fillna['']  # strip NaN values if exist
+        df['content'] = df['content'].fillna('')  # strip NaN values if exist
 
         return df
 
-    def gen_counter(self, df=None, split_words=True):
+    def gen_counter(self, df=None, split_words=False):
+        """
+        Generate Counter object from pandas series of twitch chat messages.
+
+        Parameters
+        ----------
+        df : {None, pandas.Series}, optional
+            Series containing chat messages. If parameter is left blank will
+            default to the 'content' column of the instances dataframe (i.e.
+            df['content']).
+
+        split_words : Boolean, optional
+            If true, will split each chat message into single words using
+            whitespace as delimiter. Can be useful if you're only interested
+            in single word spam like emotes. Default value is False.
+
+        Returns
+        -------
+        out : collections.Counter
+            Counter object from python collections module, using your
+            dataframe input.
+        """
         if df is None:
             words = self.df['content']
         else:
